e5e155a4b6158e1ea9056e9ceec0ced31364477e
Vivek <vivekousik8555@gmail.com>
2025-06-19 15:43:41 +0530

    done with the nativewind setup with tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white dark:bg-black">
-      <Text className="text-lg font-bold text-purple-600">
+      <Text className="text-2xl font-bold text-purple-600">
         Hello, NativeWind!
       </Text>
     </View>
