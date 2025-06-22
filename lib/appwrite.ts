93ac3e85f6d7b75abef92b555c8245960c729fa3
Vivek <vivekousik8555@gmail.com>
2025-06-22 18:11:49 +0530

    setup the backend with appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    platform: 'com.vivek.viv',
+    projectId: '6857d15c0020da21df1a',
+    databaseId: '6857d6730011b84b0f8a',
+    usersCollectionId: '6857d6c7000de13277dc',
+    videosColletionId: '6857d741003cb6d08e8c'
+}
