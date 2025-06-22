f067eee36099b6b8ada52abe97083c934779b436
Vivek <vivekousik8555@gmail.com>
2025-06-22 11:49:24 +0530

    fixed the constants error and created the bottom navigation bar

--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,5 @@
 import profile from "../assets/images/profile.png";
-import thumbnail from "../assets/images/thumbnail.png";
+import thumbnail from "../assets/images/thumbnail.jpg";
 import cards from "../assets/images/cards.png";
 import path from "../assets/images/path.png";
 import logoSmall from "../assets/images/logo-small.png";
