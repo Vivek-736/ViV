c6bb215639828deb5a3390728a993506be9cb6d1
Vivek <vivekousik8555@gmail.com>
2025-06-22 11:12:21 +0530

    done with the setup fo file and folder structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View className='flex-1 items-center justify-center bg-black-100'>
       <Text className='text-white'>Hello World!</Text>
+      <Link href={'/home'} className='text-white mt-4 text-4xl'>Home</Link>
     </View>
   );
 }
