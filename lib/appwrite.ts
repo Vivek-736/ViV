63a1f238e7a82e0d2f11e10e32a8e5249777d822
Vivek <vivekousik8555@gmail.com>
2025-06-25 15:06:52 +0530

    setup appwrite with react native sdk

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,8 +1,9 @@
 export const appwriteConfig = {
-    endpoint: 'https://cloud.appwrite.io/v1',
+    endpoint: 'https://fra.cloud.appwrite.io/v1',
     platform: 'com.vivek.viv',
     projectId: '6857d15c0020da21df1a',
     databaseId: '6857d6730011b84b0f8a',
     usersCollectionId: '6857d6c7000de13277dc',
-    videosColletionId: '6857d741003cb6d08e8c'
+    videosColletionId: '685b9115001446368109',
+    storageId: '685badee00042d0fb7ed'
 }
